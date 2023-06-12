9c487b3e44143736f261d35c1f741095c6df72b9
YoonSuLee <ysleedev01@gmail.com>
2023-06-12 18:35:03 +0900

    Initial Commit

---
+++ SWIFTUI_MovieReview/SWIFTUI_MovieReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUI_MovieReviewApp.swift
+//  SWIFTUI_MovieReview
+//
+//  Created by 이윤수 on 2023/06/12.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUI_MovieReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
